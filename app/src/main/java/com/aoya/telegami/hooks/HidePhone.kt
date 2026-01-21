4d585826064c77659627d6bac7fbcde31ee955d2
Aoya <147955401+aoya111@users.noreply.github.com>
2026-01-21 23:24:46 +0800

    change hide 'phone number' hook behaviour
    do not change user object, just change visually

--- app/src/main/java/com/aoya/telegami/hooks/HidePhone.kt
+++ app/src/main/java/com/aoya/telegami/hooks/HidePhone.kt
@@ -6,6 +6,7 @@ import com.aoya.telegami.utils.Hook
 import com.aoya.telegami.utils.HookStage
 import com.aoya.telegami.utils.hook
 import com.aoya.telegami.virt.tgnet.TLRPC
+import com.aoya.telegami.virt.ui.cells.DrawerProfileCell
 import com.aoya.telegami.core.obfuscate.ResolverManager as resolver
 
 class HidePhone :
@@ -16,11 +17,11 @@ class HidePhone :
     override fun init() {
         if (Telegami.packageName == "tw.nekomimi.nekogram") return
         findClass(
-            "org.telegram.messenger.UserConfig",
-        ).hook(resolver.getMethod("org.telegram.messenger.UserConfig", "getCurrentUser"), HookStage.AFTER) { param ->
-            val user = TLRPC.User(param.getResult() ?: return@hook)
-            val phone = if (isEnabled) "" else Config.getCurrentUser().phone
-            user.phone = phone
+            "org.telegram.ui.Cells.DrawerProfileCell",
+        ).hook(resolver.getMethod("org.telegram.ui.Cells.DrawerProfileCell", "setUser"), HookStage.AFTER) { param ->
+            val o = DrawerProfileCell(param.thisObject())
+            val user = TLRPC.User(param.arg<Any>(0))
+            o.phoneTextView.setText("@${user.username}")
         }
     }
 }
