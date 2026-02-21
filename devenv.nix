54b871b9dc68bdb7fe8af44a49563e213b9e138b
Aoya <gitya@sl.yohli.me>
2026-02-21 22:34:30 +0800

    chore: devenv

--- devenv.nix
+++ devenv.nix
@@ -1,15 +1,15 @@
-{ pkgs, lib, config, inputs, ... }:
-
-{
+{ pkgs, lib, config, inputs, ... }: {
   # https://devenv.sh/packages/
-  packages = with pkgs; [ apktool git gradle jadx ];
+  packages = with pkgs; [ gh git gradle ];
 
   # https://devenv.sh/languages/
-  android = {
-    enable = true;
-    platforms.version = [ "34" ];
-    buildTools.version = [ "34.0.0" ];
-  };
+  # android = {
+  #   enable = true;
+  #   platforms.version = [ "35" ];
+  #   buildTools.version = [ "35.0.0" ];
+  #   emulator = { enable = true; };
+  # };
+  languages.kotlin.enable = true;
 
   # https://devenv.sh/processes/
   # processes.cargo-watch.exec = "cargo-watch";
@@ -18,6 +18,12 @@
   # services.postgres.enable = true;
 
   enterShell = ''
+    export ANDROID_HOME=$HOME/android-sdk
+    export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
+    export PATH=$ANDROID_HOME/platform-tools:$PATH
+    export PATH=$ANDROID_HOME/emulator:$PATH
+    export PATH=$ANDROID_HOME/ndk:$PATH
+
     git --version
   '';
 
