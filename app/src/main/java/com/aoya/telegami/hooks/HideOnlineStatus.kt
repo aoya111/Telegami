38f22295f7bc28e0fe01619d3cbce312f53c0052
Aoya <gitya@sl.yohli.me>
2026-02-21 19:52:52 +0800

    fix: hideonlinestatus fix
    - offline attribute should be set to true
    - assigng correct nekogram class mapping

--- app/src/main/java/com/aoya/telegami/hooks/HideOnlineStatus.kt
+++ app/src/main/java/com/aoya/telegami/hooks/HideOnlineStatus.kt
@@ -2,6 +2,7 @@ package com.aoya.telegami.hooks
 
 import com.aoya.telegami.utils.Hook
 import com.aoya.telegami.utils.HookStage
+import com.aoya.telegami.utils.logd
 import com.aoya.telegami.virt.tgnet.tl.TLAccount
 import com.aoya.telegami.virt.ui.ProfileActivity
 import com.aoya.telegami.core.i18n.TranslationManager as i18n
@@ -11,23 +12,33 @@ class HideOnlineStatus :
         "hide_online_status",
         "Hide 'Online' status",
     ) {
+    private val updateStatusClass: Class<*> by lazy {
+        findClass("org.telegram.tgnet.tl.TL_account\$updateStatus")
+    }
+
     override fun init() {
         findAndHook("org.telegram.tgnet.ConnectionsManager", "sendRequestInternal", HookStage.BEFORE) { param ->
-            val tlAccountUpdateStatusClass = findClass("org.telegram.tgnet.tl.TL_account\$updateStatus")
+            val req = param.arg<Any>(0)
+            if (updateStatusClass.isInstance(req)) {
+                TLAccount.UpdateStatus(param.arg<Any>(0)).offline = true
+            }
+        }
 
-            if (tlAccountUpdateStatusClass.isInstance(param.arg<Any>(0))) {
-                TLAccount.UpdateStatus(param.arg<Any>(0)).offline = false
+        findAndHook("org.telegram.tgnet.ConnectionsManager", "sendRequestInternal", HookStage.AFTER) { param ->
+            val req = param.arg<Any>(0)
+            if (updateStatusClass.isInstance(req)) {
+                logd("[HideOnlineStatus] isOffline = ${TLAccount.UpdateStatus(param.arg<Any>(0)).offline}")
             }
         }
 
         findAndHook("org.telegram.ui.ProfileActivity", "updateProfileData", HookStage.AFTER) { param ->
             val o = ProfileActivity(param.thisObject())
 
-            val clientUserId = o.getUserConfig().getClientUserId()
+            val clientUserId = o.getUserConfig().clientUserId
             val userId = o.userId
 
             if (clientUserId != 0L && userId != 0L && userId == clientUserId) {
-                o.onlineTextView[1].setText(i18n.get("ProfileStatusOffline"))
+                o.onlineTextView.getOrNull(1)?.setText(i18n.get("ProfileStatusOffline"))
             }
         }
     }
