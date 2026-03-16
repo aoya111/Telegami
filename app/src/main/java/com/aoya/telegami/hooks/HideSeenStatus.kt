78ca63191430e548ec9fc5aff36a6ceb9491af8a
Aoya <147955401+aoya111@users.noreply.github.com>
2026-03-16 23:14:52 +0800

    feat: hideseenstatus split

--- app/src/main/java/com/aoya/telegami/hooks/HideSeenStatus.kt
+++ app/src/main/java/com/aoya/telegami/hooks/HideSeenStatus.kt
@@ -1,12 +1,26 @@
 package com.aoya.telegami.hooks
 
+import com.aoya.telegami.core.Config
 import com.aoya.telegami.utils.Hook
 import com.aoya.telegami.utils.HookStage
+import com.aoya.telegami.virt.messenger.MessagesController.ReadTask
 
 class HideSeenStatus : Hook("HideSeenStatus") {
     override fun init() {
         findAndHook("org.telegram.messenger.MessagesController", "completeReadTask", HookStage.BEFORE) { param ->
-            param.setResult(null)
+            val dialogId = ReadTask(param.arg<Any>(0)).dialogId
+            val currentUserId = Config.getCurrentUser().id
+
+            val shouldHide =
+                when {
+                    dialogId == currentUserId -> false
+                    dialogId > 0 -> Config.isFeatureEnabled("HideSeenPrivateChat")
+                    else -> Config.isFeatureEnabled("HideSeenChannel")
+                }
+
+            if (shouldHide) {
+                param.setResult(null)
+            }
         }
     }
 }
--- app/src/main/java/com/aoya/telegami/virt/messenger/MessagesController.kt
+++ app/src/main/java/com/aoya/telegami/virt/messenger/MessagesController.kt
@@ -2,6 +2,7 @@ package com.aoya.telegami.virt.messenger
 
 import com.aoya.telegami.virt.tgnet.TLRPC
 import de.robv.android.xposed.XposedHelpers.callMethod
+import de.robv.android.xposed.XposedHelpers.getLongField
 import com.aoya.telegami.core.obfuscate.ResolverManager as resolver
 
 class MessagesController(
@@ -12,4 +13,13 @@ class MessagesController(
     fun getUser(id: Long): TLRPC.User? = callMethod(instance, resolver.getMethod(objPath, "getUser"), id)?.let { TLRPC.User(it) }
 
     fun getChat(id: Long): TLRPC.Chat? = callMethod(instance, resolver.getMethod(objPath, "getChat"), id)?.let { TLRPC.Chat(it) }
+
+    class ReadTask(
+        private val instance: Any,
+    ) {
+        private val objPath = "org.telegram.messenger.MessagesController\$ReadTask"
+
+        val dialogId: Long
+            get() = getLongField(instance, resolver.getField(objPath, "dialogId"))
+    }
 }
