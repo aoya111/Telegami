c90b35f2bf3213239ba9a9a2a50d481444407556
Aoya <147955401+aoya111@users.noreply.github.com>
2026-03-19 23:58:14 +0800

    feat: enable ModifyDeletedMessageMenu

--- app/src/main/java/com/aoya/telegami/hooks/MarkMessages.kt
+++ app/src/main/java/com/aoya/telegami/hooks/MarkMessages.kt
@@ -1,34 +1,21 @@
 package com.aoya.telegami.hooks
 
-import android.text.SpannableStringBuilder
-import com.aoya.telegami.Telegami
 import com.aoya.telegami.core.Config
-import com.aoya.telegami.data.DeletedMessage
 import com.aoya.telegami.utils.Hook
 import com.aoya.telegami.utils.HookStage
 import com.aoya.telegami.utils.MessageHelper
 import com.aoya.telegami.virt.messenger.AndroidUtilities
 import com.aoya.telegami.virt.messenger.MessageObject
-import com.aoya.telegami.virt.tgnet.TLRPC
 import com.aoya.telegami.virt.ui.ChatActivity
 import com.aoya.telegami.virt.ui.actionbar.Theme
 import com.aoya.telegami.virt.ui.cells.ChatMessageCell
-import kotlinx.coroutines.launch
 import kotlin.math.ceil
-import com.aoya.telegami.core.i18n.TranslationManager as i18n
 
 class MarkMessages : Hook("MarkMessages") {
-    private val deletedMessagesCache = mutableMapOf<Pair<Long, Int>, DeletedMessage>()
-
     override fun init() {
         findAndHook("org.telegram.ui.ChatActivity", "createView", HookStage.AFTER, filter = { true }) { param ->
             val o = ChatActivity(param.thisObject())
-            Globals.coroutineScope.launch {
-                val deleted = db.deletedMessageDao().getAllForDialog(o.dialogId)
-                deleted.forEach { msg ->
-                    deletedMessagesCache[o.dialogId to msg.id] = msg
-                }
-            }
+            Globals.loadDeletedMessagesForDialog(o.dialogId)
         }
 
         findAndHook("org.telegram.ui.Cells.ChatMessageCell", "measureTime", HookStage.AFTER, filter = { true }) { param ->
@@ -36,7 +23,6 @@ class MarkMessages : Hook("MarkMessages") {
             val msgObj = MessageObject(param.arg<Any>(0))
             val dialogId = msgObj.dialogId
             val mid = msgObj.id
-            val msgKey = dialogId to mid
 
             var timeStr = msgCell.currentTimeString
             var timeTextWidth = msgCell.timeTextWidth
@@ -46,7 +32,9 @@ class MarkMessages : Hook("MarkMessages") {
             var isDeleted = false
             val oldWidth = ceil(Theme.chatTimePaint.measureText(timeStr, 0, timeStr.length)).toInt()
             if (Config.isFeatureEnabled("ShowDeletedMessages")) {
-                deletedMessagesCache[msgKey]?.let { msg ->
+                isDeleted = Globals.isDeletedMessage(dialogId, mid)
+                if (isDeleted) {
+                    val msg = Globals.getDeletedMessage(dialogId, mid)!!
                     timeStr = MessageHelper.createDeletedString(msg)
                     val newWidth = ceil(Theme.chatTimePaint.measureText(timeStr, 0, timeStr.length)).toInt()
 
@@ -59,9 +47,7 @@ class MarkMessages : Hook("MarkMessages") {
                             timeTextWidth += drawableAdjustment.toInt() + dwidth
                             timeWidth += drawableAdjustment.toInt() + 5 * dwidth / 6
                         }
-                        // timeWidth = timeTextWidth
                     }
-                    isDeleted = true
                 }
             }
             if (!isDeleted) {
--- app/src/main/java/com/aoya/telegami/hooks/ModifyDeletedMessagesMenu.kt
+++ app/src/main/java/com/aoya/telegami/hooks/ModifyDeletedMessagesMenu.kt
@@ -1,128 +1,35 @@
 package com.aoya.telegami.hooks
 
-import android.view.View
-import android.widget.LinearLayout
 import com.aoya.telegami.core.Config
 import com.aoya.telegami.utils.Hook
 import com.aoya.telegami.utils.HookStage
-import com.aoya.telegami.virt.ui.cells.ChatMessageCell
-import de.robv.android.xposed.XposedHelpers.callMethod
-import de.robv.android.xposed.XposedHelpers.getIntField
-import de.robv.android.xposed.XposedHelpers.getObjectField
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import com.aoya.telegami.virt.messenger.LocaleController
+import com.aoya.telegami.virt.messenger.MessageObject
 
 class ModifyDeletedMessagesMenu : Hook("ModifyDeletedMessagesMenu") {
-    var msgIsDeleted = false
-
-    companion object {
-        val SEEN_TYPE_SEEN = 0
-    }
-
     override fun init() {
         findAndHook(
             "org.telegram.ui.ChatActivity",
-            "lambda\$createMenu$290",
+            "fillMessageMenu",
             HookStage.BEFORE,
             filter = {
                 Config.isFeatureEnabled("ShowDeletedMessages")
             },
         ) { param ->
-            if (!msgIsDeleted) return@findAndHook
-            val o = param.thisObject()
+            val msgObj = MessageObject(param.arg<Any>(0))
+            val arrayList = param.arg<ArrayList<Int>>(1)
+            val arrayList2 = param.arg<ArrayList<String>>(2)
+            val arrayList3 = param.arg<ArrayList<Int>>(3)
 
-            val scrimPopupWindow = getObjectField(o, "scrimPopupWindow")
-            val scrimPopupContainerLayout = callMethod(scrimPopupWindow, "getContentView")
-
-            val actionBarPopupWindowLayoutClass =
-                findClass("org.telegram.ui.ActionBar.ActionBarPopupWindow\$ActionBarPopupWindowLayout")
-
-            var popupLayout: Any? = null
-            scrimPopupContainerLayout.let { l ->
-                if (l is LinearLayout) {
-                    val viewsToRemove = mutableListOf<View>()
-                    val childCount = l.childCount
-                    for (i in 0 until childCount) {
-                        val child = l.getChildAt(i)
-                        if (actionBarPopupWindowLayoutClass.isInstance(child)) {
-                            popupLayout = child
-                            break
-                        } else {
-                            viewsToRemove.add(child)
-                        }
-                    }
-                    viewsToRemove.forEach { l.removeView(it) }
-                }
+            if (Globals.isDeletedMessage(msgObj.dialogId, msgObj.id)) {
+                arrayList2.add(LocaleController.getString(getResource("Copy", "string")))
+                arrayList2.add(LocaleController.getString(getResource("Delete", "string")))
+                arrayList3.add(3)
+                arrayList3.add(1)
+                arrayList.add(getResource("msg_copy", "drawable"))
+                arrayList.add(getResource("msg_delete", "drawable"))
+                param.setResult(null)
             }
-
-            val actionBarMenuSubItemClass = findClass("org.telegram.ui.ActionBar.ActionBarMenuSubItem")
-            val messagePrivateSeenView = findClass("org.telegram.ui.Components.MessagePrivateSeenView")
-            val gapView = findClass("org.telegram.ui.ActionBar.ActionBarPopupWindow\$GapView")
-            val allowedItems = listOf(getStringResource("Copy"), getStringResource("Delete"))
-            popupLayout?.let { p ->
-                val viewsToRemove = mutableListOf<View>()
-                val l = getObjectField(p, "linearLayout") as LinearLayout
-                val childCount = l.childCount
-                for (i in 0 until childCount) {
-                    val child = l.getChildAt(i)
-                    if (messagePrivateSeenView.isInstance(child)) {
-                        if (getIntField(child, "type") == SEEN_TYPE_SEEN) {
-                            viewsToRemove.add(child)
-                        }
-                    } else if (actionBarMenuSubItemClass.isInstance(child)) {
-                        val textView = getObjectField(child, "textView")
-                        val text = callMethod(textView, "getText") as? String ?: continue
-                        if (text !in allowedItems) {
-                            viewsToRemove.add(child)
-                        }
-                    }
-                }
-                viewsToRemove.forEach { l.removeView(it) }
-                if (gapView.isInstance(l.getChildAt(0))) {
-                    l.removeViewAt(0)
-                }
-            }
-        }
-
-        findAndHook(
-            "org.telegram.ui.ChatActivity",
-            "createMenu",
-            HookStage.BEFORE,
-            filter = {
-                Config.isFeatureEnabled("ShowDeletedMessages")
-            },
-        ) { param ->
-            val p1 = param.arg<Any>(0)
-            val chatMessageCellClass = findClass("org.telegram.ui.Cells.ChatMessageCell")
-            if (!chatMessageCellClass.isInstance(p1)) return@findAndHook
-            val chatMsgCellObj = ChatMessageCell(p1)
-
-            val msgObj = chatMsgCellObj.getMessageObject()
-
-            // val type = callMethod(o, "getMessageType", msg) as Int
-            // if (type != 3) return@hook
-
-            msgIsDeleted = false
-            runBlocking {
-                launch {
-                    db.deletedMessageDao().get(msgObj.id, msgObj.dialogId)?.let {
-                        msgIsDeleted = true
-                    }
-                }.join()
-            }
-        }
-
-        findAndHook(
-            "org.telegram.ui.Components.MessagePrivateSeenView",
-            "request",
-            HookStage.BEFORE,
-            filter = {
-                Config.isFeatureEnabled("ShowDeletedMessages")
-            },
-        ) { param ->
-            val o = param.thisObject()
-            if (!msgIsDeleted || (getIntField(o, "type") != SEEN_TYPE_SEEN)) return@findAndHook
-            param.setResult(null)
         }
     }
 }
