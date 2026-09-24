e9a49c30f2525364262d2396f66c9bf9b38fc225
Aoya <gitya@sl.yohli.me>
2026-09-25 00:00:56 +0800

    feat: improve disable ads hook

--- app/src/main/java/com/aoya/telegami/hooks/DisableAds.kt
+++ app/src/main/java/com/aoya/telegami/hooks/DisableAds.kt
@@ -1,6 +1,8 @@
 package com.aoya.telegami.hooks
 
+import android.content.SharedPreferences
 import android.util.Log
+import com.aoya.telegami.core.obfuscate.ResolverManager
 import com.aoya.telegami.service.Config
 import com.aoya.telegami.util.findMethod
 import io.github.libxposed.api.XposedInterface
@@ -14,26 +16,66 @@ object DisableAds {
         classLoader: ClassLoader,
     ) {
         if (!Config.isFeatureEnabled("DisableAds")) return
-        listOf(
-            classLoader.findMethod(CHAT_ACTIVITY_CN, "addSponsoredMessages") to null,
-            classLoader.findMethod(MESSAGES_CONTROLLER_CN, "getSponsoredMessages") to null,
-        ).forEach { (method, value) ->
-            xposed.hook(method).intercept { chain ->
-                val args = chain.args.toMutableList()
-                var result: Any? = null
-                var hasResult = false
+        installSponsoredHook(xposed, "addSponsoredMessages") {
+            classLoader.findMethod(CHAT_ACTIVITY_CN, "addSponsoredMessages")
+        }
+        installSponsoredHook(xposed, "getSponsoredMessages") {
+            classLoader.findMethod(MESSAGES_CONTROLLER_CN, "getSponsoredMessages")
+        }
+        installProxySponsorHook(xposed, classLoader)
+    }
+
+    private fun installSponsoredHook(
+        xposed: XposedInterface,
+        name: String,
+        resolve: () -> java.lang.reflect.Method,
+    ) {
+        try {
+            xposed.hook(resolve()).intercept { null }
+        } catch (throwable: Throwable) {
+            Log.w("Telegami", "Unable to install $name", throwable)
+        }
+    }
+
+    private fun installProxySponsorHook(
+        xposed: XposedInterface,
+        classLoader: ClassLoader,
+    ) {
+        try {
+            val controllerClass = classLoader.loadClass(ResolverManager.get(MESSAGES_CONTROLLER_CN))
+            val promoCheck =
+                controllerClass.declaredMethods.firstOrNull {
+                    it.name.startsWith("lambda\$checkPromoInfo") && it.parameterCount == 1
+                } ?: return
+            val removePromoDialog =
+                controllerClass.declaredMethods.firstOrNull {
+                    it.name == "removePromoDialog" && it.parameterCount == 0
+                } ?: return
+            val globalSettings =
+                controllerClass.declaredMethods.firstOrNull {
+                    it.name == "getGlobalMainSettings" && it.parameterCount == 0
+                } ?: return
+
+            promoCheck.isAccessible = true
+            removePromoDialog.isAccessible = true
+            globalSettings.isAccessible = true
+            xposed.hook(promoCheck).intercept { chain ->
+                val result = chain.proceed()
                 try {
-                    result = value
-                    hasResult = true
+                    removePromoDialog.invoke(chain.thisObject)
+                    (globalSettings.invoke(null) as? SharedPreferences)
+                        ?.edit()
+                        ?.remove("proxy_dialog")
+                        ?.remove("proxyDialogAddress")
+                        ?.remove("nextPromoInfoCheckTime")
+                        ?.commit()
                 } catch (throwable: Throwable) {
-                    Log.e("Telegami", "Hook callback failed before ${method.name}", throwable)
-                }
-                if (!hasResult) {
-                    result = chain.proceed(args.toTypedArray())
-                    hasResult = true
+                    Log.e("Telegami", "Failed to hide proxy sponsor", throwable)
                 }
                 result
             }
+        } catch (throwable: Throwable) {
+            Log.w("Telegami", "Unable to install proxy sponsor hook", throwable)
         }
     }
 }
